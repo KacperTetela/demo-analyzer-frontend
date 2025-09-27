55dc9a5c2ae6e6203197acd4e3bbcebccffc3eb7
KacperTetela <kacpertetela1@gmail.com>
2025-09-27 19:09:15 +0200

    add integration with side sites

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -2,6 +2,10 @@ const sidebar = document.getElementById('sidebar');
 const toggleTheme = document.getElementById('theme-toggle');
 const collapseBtn = document.querySelector('.toggle-sidebar');
 const collapseIcon = document.getElementById('collapse-icon');
+const homeBtn = document.getElementById('home-btn');
+const historyBtn = document.getElementById('history-btn');
+const accountBtn = document.getElementById('account-btn');
+const loginBtn = document.getElementById('login-btn');
 
 // Sprawdzanie, czy elementy istnieją, przed dodaniem detektora zdarzeń
 if (toggleTheme) {
@@ -23,3 +27,23 @@ if (collapseBtn) {
         }
     });
 }
+
+homeBtn.addEventListener('click', () => {
+    console.log('Home button clicked');
+    window.location.href = 'index.html';
+});
+
+historyBtn.addEventListener('click', () => {
+    console.log('History button clicked');
+    window.location.href = 'demohistory.html';
+});
+
+accountBtn.addEventListener('click', () => {
+    console.log('Account button clicked');
+    window.location.href = 'account.html';
+});
+
+loginBtn.addEventListener('click', () => {
+    console.log('Login button clicked');
+    window.location.href = 'login.html';
+});
