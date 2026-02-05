9edb8ac531b22a32c98ce108652cec15c6102d4b
KacperTetela <kacpertetela1@gmail.com>
2026-02-06 00:40:32 +0100

    feat: implement API utility for making authenticated requests and managing tokens.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://develop.api.demoanalyzer.kacpertetela.ddns.net';
 
 export const request = async (endpoint, options = {}) => {
     const token = localStorage.getItem('jwt_token');
